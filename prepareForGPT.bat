bb1b55279b2b52c7fce93c20032df03a5b089488
Gabriel <gki19001@student.mdu.se>
2024-01-23 13:25:45 +0100

    <message goes here>

---
+++ prepareForGPT.bat
@@ -0,0 +1,23 @@
+@echo off
+cls
+SET /A RAND=%RANDOM%%%9000000+1000
+echo %RAND%
+
+set scriptpath=%~dp0
+if not exist "%scriptpath%\%RAND%" mkdir "%scriptpath%\%RAND%" 
+if not exist "%scriptpath%\%RAND%\%RAND%_oracle" mkdir "%scriptpath%\%RAND%\%RAND%_oracle" 
+
+ren "%scriptpath%\input_xmi.xmi" "%RAND%_input.xmi"
+
+move "%scriptpath%\%RAND%_input.xmi" "%scriptpath%\%RAND%" 
+
+python "%scriptpath%\combineOracle.py" %*
+timeout 1 > NUL
+
+ren "%scriptpath%\input_oracle.java" "%RAND%_oracle.java"
+move "%scriptpath%\%RAND%_oracle.java" "%scriptpath%\%RAND%\\%RAND%_oracle" 
+
+for %%f in (*.java) do (
+    if "%%~xf"==".java" echo %%f & del %%f
+)
+

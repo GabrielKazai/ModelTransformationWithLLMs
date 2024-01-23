169e9c8df2d21d356dabb9700656691aa0ba88ff
Gabriel <gki19001@student.mdu.se>
2024-01-23 15:20:41 +0100

    this is a message

---
+++ 26208/26208_output.java
@@ -0,0 +1,28 @@
+The corresponding Java code for the given XMI code is as follows:
+
+```java
+public class City {
+    private Houses[] houses;
+}
+
+public class Houses {
+    private Family family;
+}
+
+public class Family {
+    private Father father;
+    private Mother mother;
+    private Child[] children;
+}
+
+public class Father {
+}
+
+public class Mother {
+}
+
+public class Child {
+}
+```
+
+Note: The provided XMI code only describes the class structure and associations, but does not include any attributes or methods. Therefore, the corresponding Java code only includes the class definitions without any additional details.

1dc6850006f10dffc5df23a875269dd2d3e42408
Gabriel <gki19001@student.mdu.se>
2024-01-23 15:20:30 +0100

    this is a message

---
+++ 26208/26208_oracle/26208_oracle.java
@@ -0,0 +1,34 @@
+public class Child {
+
+}
+
+public class City {
+
+	private Houses[] houses;
+
+}
+
+public class Family {
+
+	private Father Father;
+
+	private Mother Mother;
+
+	private Child[] Children;
+
+}
+
+public class Father {
+
+}
+
+public class Houses {
+
+	private Family family;
+
+}
+
+public class Mother {
+
+}
+

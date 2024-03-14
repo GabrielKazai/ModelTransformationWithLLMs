371c995b532c3de57256145ab0255819f78e4bc1
msronnie <69766424+msronnie@users.noreply.github.com>
2024-03-14 22:22:38 +0100

    xmi sort

---
+++ allXMI/works/0PrimitiveTypes/oracle/Real.java
@@ -0,0 +1,7 @@
+import com.modeliosoft.modelio.javadesigner.annotations.DataType;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@DataType
+@objid ("e85aa62e-6688-4c71-924d-7a4e934ee832")
+public class Real {
+}

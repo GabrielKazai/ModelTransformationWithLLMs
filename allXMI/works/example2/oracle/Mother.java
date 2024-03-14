4cae1f8c6d2d28af3651c0daae00572ec7981788
Gabriel <gki19001@student.mdu.se>
2024-03-14 12:01:03 +0100

    updated structure on projects

--- allXMI/works/example2/oracle/Mother.java
+++
@@ -1,5 +0,0 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("01676551-b8fe-4c1d-9679-5958b3f084e4")
-public class Mother {
-}
--- allXMI/works/example4/oracle/Child.java
+++
@@ -1,5 +0,0 @@
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("f2f43d7c-e347-4bd7-bbc0-9f797f9b28d8")
-public class Child {
-}

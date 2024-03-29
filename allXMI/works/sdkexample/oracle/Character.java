67f2b2450fc6856acc3b4c7256d638457a471960
Gabriel <gki19001@student.mdu.se>
2024-03-29 12:48:43 +0100

    creating file structure

---
+++ allXMI/works/sdkexample/oracle/Character.java
@@ -0,0 +1,5 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("e5b3466f-efeb-42dd-a1d3-44370957d999")
+public class Character {
+}
---
+++ allXMI/works/sdkexample/oracle/Float.java
@@ -0,0 +1,5 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("45373ce1-0146-4127-a068-16c59cf1e35c")
+public class Float {
+}

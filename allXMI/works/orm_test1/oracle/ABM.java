83d2df5ef031ebe64c42602819eee6932b944d15
Gabriel <gki19001@student.mdu.se>
2024-03-29 13:03:44 +0100

    executing with gpt

---
+++ allXMI/works/orm_test1/oracle/ABM.java
@@ -0,0 +1,215 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("0a191190-d276-4f69-9c0c-4eab6b4bfaae")
+class ABM extends ORM {
+    @objid ("1df7bd13-2aa9-4d5c-9edd-f119a8998f20")
+    public void __construct(final mixed $tabla) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("a567a48b-fa03-4e7b-9f0b-95db8310fd37")
+    public void add_mensaje(final mixed $msg) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("9df65585-be0e-47c6-9aac-3b0bff78e9f7")
+    public void get_mensajes() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("ec06765c-3c91-4ce6-b660-ee6377c9bd72")
+    public void get_pagina_actual() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("ac341226-a045-44fc-a32f-a30e707742a1")
+    public void get_items_por_pagina() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("b2198e37-2271-467f-9fd9-8df74e9d6c2f")
+    public void set_pagina_actual(final mixed $val) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("91520294-d1b9-411f-a2f9-26ea2a91f0e9")
+    public void set_items_por_pagina(final mixed $val) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("fdb83c95-40d0-410d-8d07-7e1f021cfa2b")
+    public void save() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("51c97722-24b0-4e83-ad2c-89ed4acf38b8")
+    public void baja(final mixed $id, final bool $logica, final mixed $data) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("3b3561f1-561f-4ab5-ab5f-3f9d88b697d2")
+    public void reactivar(final mixed $id, final mixed $data) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("79dd15ee-b244-43b6-a772-0fa819552151")
+    public void set_force_insert(final mixed $val) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("befe7f95-dcc3-4a06-8781-db2f0273f43a")
+    public void set_campo_fecha_vigencia(final mixed $val) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("80a07466-4b86-4473-a80e-9bc02b55f64f")
+    public void get_campo_fecha_vigencia() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("abdca9e7-a86d-4db1-9139-2e3690720128")
+    public void tiene_valor_en_campo_id() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("712215bd-53a2-4a1c-8cba-f70890c12ceb")
+    public void is_new_item() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("e223214d-486b-49a0-b56e-3b072009cd96")
+    public void tiene_fecha_vigencia() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("941e5f64-a80d-4412-b2ba-72b811aa03b1")
+    public void requiere_nueva_vigencia() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("cab71ce1-beed-4f07-9db9-0a9550ebd548")
+    public void validate() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("175c6c3b-f713-4ac8-ad59-69907e8466bc")
+    public void get_rotulos_from_field_names(final mixed $fns) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("540c4f48-5cc1-47cb-878f-fd87e4b872da")
+    public void load_fields_from_array(final mixed $arr) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("75b68d1c-f706-49fe-a5ca-d37fd7763872")
+    public void set_form_buttons(final mixed $arr) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("122c37a9-f159-4018-9481-43a2c835e45d")
+    public void get_form_buttons() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("56d80bb0-d3fe-4753-9c99-e899837d23ef")
+    public void add_form_button(final mixed $button, final bool $ultimo) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("9ef9a1df-c2ef-4435-9828-3666fb5df250")
+    public void set_request_data(final mixed $data) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("b0f96fd8-e806-4f74-b058-6c22d4516b7c")
+    public void get_request_data() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("c1238c09-654f-4eca-9508-2e9382cce2d5")
+    public void render_form(final mixed $data) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("b451a925-029a-44fb-ad35-86bdf749b699")
+    public void render_form_html(final mixed $data) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("3d3bb37d-8c2f-47c6-876d-e5be2aa5743e")
+    public void render_form_json() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("c4e5eb88-b35c-46a8-896b-3714282c0219")
+    public void add_code_to_form(final mixed $str, final bool $arriba) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("2183a2b3-fe65-4403-8da8-e5b09acb4355")
+    public void set_form_columnas(final int $int) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("1fb91c5a-eaff-449e-b87a-dc705d385368")
+    public void set_form_titulo(final mixed $str) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("a42dae77-219f-442e-9813-0bc95e18b3d3")
+    public void analizar_operacion(final mixed $data) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("b1219d7c-5407-436c-8162-3dacff3d1fbb")
+    public void set_operacion(final mixed $op) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("f05da721-8e76-44f6-9f44-8537990e3921")
+    public void get_operacion() {
+        // TODO Auto-generated return
+        return null;
+    }
+
+    @objid ("44191c8c-1ca1-4116-9dc1-681e680e6043")
+    public void search(final mixed $criterios, final mixed $campos, final mixed $campo_id, final mixed $paginado) {
+        // TODO Auto-generated return
+        return null;
+    }
+
+}

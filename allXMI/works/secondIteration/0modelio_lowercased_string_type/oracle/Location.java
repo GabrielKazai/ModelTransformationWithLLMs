3fe06f5a5f1f340f2c45fad31c8d2ca7231efe88
Gabriel <gki19001@student.mdu.se>
2024-04-04 15:25:50 +0200

    Revert "Merge pull request #13 from GabrielKazai/seconditerationtest"
    This reverts commit 18330461347372a3109043b94de940f3c14563d5, reversing
    changes made to 008d9a0e7cc76e62d949a5548416c7d92d62261c.

---
+++ allXMI/works/secondIteration/0modelio_lowercased_string_type/oracle/Location.java
@@ -0,0 +1,23 @@
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("8538b671-8eed-407e-8c14-6133accced31")
+public class Location {
+    @objid ("cd6c44cf-7b9f-4712-bcd2-abe038b02a86")
+    private long locationId;
+
+    @objid ("546bb6da-be0b-478f-9aaf-365805bd041e")
+    private String streetAddress;
+
+    @objid ("6a3982fc-8c07-426f-bd63-f848436c08c0")
+    private String postalCode;
+
+    @objid ("e12e21b3-80f3-4d5e-99cd-f70954a36c30")
+    private String city;
+
+    @objid ("b424548c-4546-4620-a2f4-7eb28cb324ec")
+    private String stateProvince;
+
+    @objid ("1b40b4a5-2bcd-4ec5-b18e-86bce2079b48")
+    private Country country;
+
+}

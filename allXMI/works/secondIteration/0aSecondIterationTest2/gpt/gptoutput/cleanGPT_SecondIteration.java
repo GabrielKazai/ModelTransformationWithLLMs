d9809dd5eae4addc54ea1e3205beecf1cd4dc1c7
Gabriel <gki19001@student.mdu.se>
2024-04-05 14:50:20 +0200

    testing iteration 2

---
+++ allXMI/works/secondIteration/0aSecondIterationTest2/gpt/gptoutput/cleanGPT_SecondIteration.java
@@ -0,0 +1,22 @@
+public class City {
+    private List<Houses> Houses;
+}
+
+public class Houses {
+    private Family Family;
+}
+
+public class Family {
+    private Father Father;
+    private Mother Mother;
+    private List<Child> Children;
+}
+
+public class Father {
+}
+
+public class Mother {
+}
+
+public class Child {
+}

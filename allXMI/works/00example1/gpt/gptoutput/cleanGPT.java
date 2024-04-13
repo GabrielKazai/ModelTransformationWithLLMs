8a3153d866d938b4ea29a7c569e16a9d245fd130
Gabriel <gki19001@student.mdu.se>
2024-04-13 13:26:08 +0200

    preparing for second iteration

--- allXMI/works/00example1/gpt/gptoutput/cleanGPT.java
+++ allXMI/works/00example1/gpt/gptoutput/cleanGPT.java
@@ -1,5 +1,6 @@
-// City.javapublic class City {private Houses[] houses;}// Houses.javapublic
-// class Houses {private Family family;}// Family.javapublic class Family
-// {private Father father;private Mother mother;private Child[] children;}//
-// Father.javapublic class Father {}// Mother.javapublic class Mother {}//
-// Child.javapublic class Child {}
+// File: City.javapublic class City {private Houses[] houses;}// File:
+// Houses.javapublic class Houses {private Family family;}// File:
+// Family.javapublic class Family {private Father father;private Mother
+// mother;private Child[] children;}// File: Father.javapublic class Father {}//
+// File: Mother.javapublic class Mother {}// File: Child.javapublic class Child
+// {}
---
+++ allXMI/works/secondIteration/00example3/gpt/gptoutput/cleanGPT_SecondIteration.java
@@ -0,0 +1,24 @@
+
+public class City {
+
+    private Houses Houses;
+}
+
+public class Houses {
+    private Family Family;
+}
+
+public class Family {
+    private Father Father;
+    private Mother Mother;
+    private List<Child> Children;
+}
+
+public class Father {
+}
+
+public class Mother {
+}
+
+public class Child {
+}

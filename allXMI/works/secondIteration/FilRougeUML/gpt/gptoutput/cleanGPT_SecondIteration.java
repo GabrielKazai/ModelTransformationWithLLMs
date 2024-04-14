68d763729dd000198c0f32bb26cefab7d583f3af
Gabriel <gki19001@student.mdu.se>
2024-04-14 14:21:04 +0200

    executing second iteration

---
+++ allXMI/works/secondIteration/FilRougeUML/gpt/gptoutput/cleanGPT_SecondIteration.java
@@ -0,0 +1,95 @@
+public class Competence {
+    private final long id;
+    private final String libelle;
+    private final boolean active;
+    private final Date dateCreation;
+}
+
+public class Personne {
+    private final long id;
+    private final String nom;
+    private final String prenom;
+    private final String email;
+    private final String img;
+    private Droit droit;
+    private List<Cursus> cursus;
+    private Etat etatPersonne;
+    private List<Competence> competences;
+
+    public void ajouterCompetence() {
+    }
+
+    public void evaluerCompetence() {
+    }
+
+    public void Operation() {
+    }
+}
+
+public class Droit {
+    private final long id;
+    private final String libelle;
+    private final int niveau;
+}
+
+public class Cursus {
+    private final long id;
+    private final Date annee;
+    private final String libelle;
+    private List<Ecole> ecole;
+}
+
+public class Ecole {
+    private final String nom;
+    private final String ville;
+}
+
+public class Note {
+    private final Personne evalue;
+    private final double note;
+}
+
+public class Projet {
+    private final long id;
+    private final String libelle;
+    private final String img;
+    private Etat etat;
+    private Etat recrutement;
+    private WikiProjet wikiProjet;
+    private WikiOfficiel wikiOfficiel;
+}
+
+public abstract class Wiki {
+    private final long id;
+    private final String contenu;
+}
+
+public class WikiProjet extends Wiki {
+}
+
+public class WikiOfficiel extends Wiki {
+}
+
+public abstract class Notification {
+    private final long id;
+    private final Date dateResolution;
+}
+
+public class Etat {
+    private final String ENUM;
+}
+
+public class NotificationAjoutComp extends Notification {
+    private final Competence competence;
+    private final Personne demandeur;
+}
+
+public class NotificationDemandeParticipation extends Notification {
+    private final Personne invite;
+    private final Projet projet;
+}
+
+public class NotificationInvitationProjet {
+    private final Personne invite;
+    private final Projet projet;
+}
---
+++ allXMI/works/secondIteration/testing/gpt/gptoutput/cleanGPT_SecondIteration.java
@@ -0,0 +1,76 @@
+public enum VisibilityKind {    public,    private,    protected,    package}
+
+public abstract class BaseClass {
+    public boolean readWriteSingleValuedPrimitiveAttribute = true;
+    public UsedClass subsettedReadWriteAttribute;
+    public UsedClass subsettedReadOnlyAttribute;
+    public boolean[] slot = new boolean[0];
+    public boolean[] readWriteDerivedAttribute = new boolean[0];
+    public boolean[] readOnlyDerivedAttribute = new boolean[0];
+    public boolean readWriteDerivedUnionAttribute;
+    public boolean readOnlyDerivedUnionAttribute;
+
+    public boolean[] readWriteDerivedAttribute() {
+        return new boolean[0];
+    }
+
+    public boolean[] readOnlyDerivedAttribute() {
+        return new boolean[0];
+    }
+
+    public boolean[] aRealOperation() {
+        return new boolean[0];
+    }
+}
+
+public class UsedClass {
+    public Class oppositeReadWriteSingleValuedObjectAttribute;
+}
+
+public class Class extends BaseClass {
+    public UsedClass readWriteSingleValuedObjectAttribute;
+    public UsedClass[] subsetReadWriteAttribute = new UsedClass[0];
+    public UsedClass subsetReadOnlyAttribute;
+    public boolean readWriteSingleValuedPrimitiveAttribute;
+    public boolean readOnlySingleValuedPrimitiveAttribute;
+    public boolean[] readWriteMultiValuedPrimitiveAttribute = new boolean[0];
+    public boolean[] readOnlyMultiValuedPrimitiveAttribute = new boolean[0];
+    public VisibilityKind[] readWriteMultiValuedEnumerationAttribute = new VisibilityKind[0];
+    public VisibilityKind[] readOnlyMultiValuedEnumerationAttribute = new VisibilityKind[0];
+    public UsedClass readOnlySingleValuedObjectAttribute;
+    public UsedClass readWriteMultiValuedObjectAttribute;
+    public UsedClass readOnlyMultiValuedObjectAttribute;
+
+    public boolean opReturnPrimitiveSV() {
+        return false;
+    }
+
+    public boolean[] opReturnPrimitiveMV() {
+        return new boolean[0];
+    }
+
+    public VisibilityKind opReturnEnumerationSV() {
+        return null;
+    }
+
+    public VisibilityKind[] opReturnEnumerationMV() {
+        return new VisibilityKind[0];
+    }
+
+    public UsedClass opReturnObjectSV() {
+        return null;
+    }
+
+    public UsedClass opReturnObjectMV() {
+        return null;
+    }
+
+    public boolean opParameters(boolean pinprimitiveSV, VisibilityKind pinenumerationSV, UsedClass pinobjectSV,
+            boolean[] pinprimitiveMV, VisibilityKind[] pinenumerationMV, UsedClass[] pinobjectMV,
+            boolean pinoutprimitiveSV, VisibilityKind pinoutenumerationSV, UsedClass pinoutobjectSV,
+            boolean[] pinoutprimitiveMV, VisibilityKind[] pinoutenumerationMV, UsedClass[] pinoutobjectMV,
+            boolean poutprimitiveSV, VisibilityKind poutenumerationSV, UsedClass poutobjectSV,
+            boolean[] poutprimitiveMV, VisibilityKind[] poutenumerationMV, UsedClass[] poutobjectMV) {
+        return false;
+    }
+}

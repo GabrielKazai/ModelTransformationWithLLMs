78c272a0f0f7feb814bd23402c95f871fba317f6
Gabriel <gki19001@student.mdu.se>
2024-04-16 14:22:55 +0200

    third iteration

---
+++ allXMI/works/secondIteration/thirdIteration/done/NIEM-Reference-common-sar/gpt/gptoutput/cleanGPT_ThirdIteration.java
@@ -0,0 +1,42 @@
+public enum TargetSectorCodeSimpleType {
+    EMRG, TELCM, SPCFXN, LIVSTK, OTHR, WATR, EDU, COMM, AGRI, HAZMAT, CHEM, CORR, NATRL, CSTL, PRSN, TACTCP, HLTH,
+    GVTBLDG, MAIL, RETL, ENTMT, CULTR, TRANS, NCLR, MNMT, POP, FIN, DFNSE, DAMS, ENRG, IT, MASS, MFG, UTL
+}
+
+public enum TargetSectorCodeType {
+    EMRG, TELCM, SPCFXN, LIVSTK, OTHR, WATR, EDU, COMM, AGRI, HAZMAT, CHEM, CORR, NATRL, CSTL, PRSN, TACTCP, HLTH,
+    GVTBLDG, MAIL, RETL, ENTMT, CULTR, TRANS, NCLR, MNMT, POP, FIN, DFNSE, DAMS, ENRG, IT, MASS, MFG, UTL
+}
+
+public enum TipClassCodeSimpleType {
+    FUND, CYBER, SPCSSBST, BIO, FRAUD, HATE, CNTFTG, INFRA, SPCSA, VRUS, SPCSP, EXPL, CTMN, INCEND, NCLR, LAUND, CHEM,
+    THRTPUB
+}
+
+public enum TipClassCodeType {
+    FUND, CYBER, SPCSSBST, BIO, FRAUD, HATE, CNTFTG, INFRA, SPCSA, VRUS, SPCSP, EXPL, CTMN, INCEND, NCLR, LAUND, CHEM,
+    THRTPUB
+}
+
+public enum TipDomainCodeSimpleType {
+    G, D, F, H, E, A, B, C
+}
+
+public enum TipDomainCodeType {
+    G, D, F, H, E, A, B, C
+}
+
+public enum ContentKind {
+    Unspecified, Empty, Simple, Mixed, ElementOnly
+}
+
+public enum FeatureKind {
+    Unspecified, Simple, Attribute, Element, AttributeWildcard, ElementWildcard, Group
+}
+
+public enum VisibilityKind {
+    Unspecified, None, ReadOnly, ReadWrite, ReadOnlyUnsettable, ReadWriteUnsettable
+}
+
+class NamespaceElementPropertyHolder {
+}

94b7b864e8394b8e63818fca9dece7e4b10d8ab5
Gabriel <gki19001@student.mdu.se>
2024-05-29 13:11:20 +0200

    .

--- allXMI/works/demo/gpt/gptoutput/cleanGPT.java
+++ allXMI/works/demo/gpt/gptoutput/cleanGPT.java
@@ -1,1 +1,22 @@
-// City.javapublic class City {private Houses[] houses;}// Houses.javapublic class Houses {private Family family;}// Family.javapublic class Family {private Father father;private Mother mother;private Child[] children;}// Father.javapublic class Father {}// Mother.javapublic class Mother {}// Child.javapublic class Child {}
+public class City {
+    private Houses[] houses;
+}
+
+public class Houses {
+    private Family family;
+}
+
+public class Family {
+    private Father father;
+    private Mother mother;
+    private Child[] children;
+}
+
+public class Father {
+}
+
+public class Mother {
+}
+
+public class Child {
+}
